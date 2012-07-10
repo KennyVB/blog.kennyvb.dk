ad0814a09fa4a805f7f9b63b90cd68cc53bc606f
Kennyvb <kennyvonbulow@gmail.com>
2012-07-10 20:56:26 +0200

    moved php footer call

--- wp-content/themes/Kennyvb 5.0/footer.php
+++ wp-content/themes/Kennyvb 5.0/footer.php
@@ -24,14 +24,7 @@
 			<a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
 			<a href="http://wordpress.org/" title="Semantic Personal Publishing Platform" rel="generator">Proudly powered by WordPress </a>
 
-<?php
-	/* Always have wp_footer() just before the closing </body>
-	 * tag of your theme, or you will break many plugins, which
-	 * generally use this hook to reference JavaScript files.
-	 */
 
-	wp_footer();
-?>
 <div class="admin">
 <?php wp_loginout(); ?> &middot; <?php wp_register('', ''); ?>
 </div>
@@ -46,5 +39,13 @@
 </div>
 
 </div>
+<?php
+	/* Always have wp_footer() just before the closing </body>
+	 * tag of your theme, or you will break many plugins, which
+	 * generally use this hook to reference JavaScript files.
+	 */
+
+	wp_footer();
+?>
 </body>
 </html>
